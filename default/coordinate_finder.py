8d7624657ac3a11645753a85cb293a58e44be358
Izhan Alam <izhan.alam1@ucalgary.ca>
2021-04-06 02:56:50 -0700

    fkin import

--- default/coordinate_finder.py
+++ default/coordinate_finder.py
@@ -1,5 +1,5 @@
 import cv2
-from get_pixel_coordinates import BoundingBoxFinder, TakeScreenshot
+from .get_pixel_coordinates import BoundingBoxFinder, TakeScreenshot
 from setup import parser
 
 '''
