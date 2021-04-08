c8c25e0acd5039756a9e5517bcc03ab930eeb6c0
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-07 21:16:58 -0700

    Update coordinate_finder.py

--- default/coordinate_finder.py
+++ default/coordinate_finder.py
@@ -21,18 +21,14 @@ Get bounding box/pixel positions
 filepath = 'images/TestImage.jpg'
 finder = BoundingBoxFinder(filepath)
 
-while True:
-    img, x,y,w,h = finder.show_image()
-    cv2.imshow('Reference Image', img)
-    print(x)
-    print(y)
-    print(w)
-    print(h)
-    key = cv2.waitKey(1)
-    # Close program with keyboard 'q'
-    if key == ord('q'):
-        cv2.destroyAllWindows()
-        exit(1)
+
+img, x,y,w,h = finder.show_image()
+cv2.imshow('Reference Image', img)
+print(x)
+print(y)
+print(w)
+print(h)
+
 
 
     
