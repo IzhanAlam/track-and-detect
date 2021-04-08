c6bc62409c4d7cf224458aebeac3b31212c16bee
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-08 01:10:43 -0700

    Update initializer.py

--- initializer.py
+++ initializer.py
@@ -40,7 +40,7 @@ def frame_setup(frame):
 
     count_by_poly = True
     show_poly = True
-    poly_outside = False
+    poly_outside = True
     if count_by_poly:
         x,y,w,h = get_box()
         poly_points =  [(x,y+h),(x+w, y+h), (x+w,y), (x, y)]
@@ -50,11 +50,11 @@ def frame_setup(frame):
     '''
     Tracking Range
     '''
-    set_obj_range = False
+    set_obj_range = True
     if set_obj_range:
         x,y,w,h = get_box()
         obj_range =  [(x,y+h),(x+w, y+h), (x+w,y), (x, y)]
-        show_obj_range = True
+        show_obj_range = False
     else:
         obj_range = None
         show_obj_range = False
