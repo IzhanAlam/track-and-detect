e9aa752f964d7df8ef7e8f74c17aeb7d39c2ee06
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-07 23:13:18 -0700

    Update initializer.py

--- initializer.py
+++ initializer.py
@@ -43,7 +43,7 @@ def frame_setup(frame):
     poly_outside = False
     if count_by_poly:
         x,y,w,h = get_box()
-        poly_points =  [(x,y),(x+h, y+w), (x,w), (y, h)]
+        poly_points =  [(x,y+h),(x+w, y+h), (x+w,y), (x, y)]
     else:
         poly_points = None
     
