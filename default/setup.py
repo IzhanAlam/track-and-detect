1b3bf11ec4cbdb157fd9d5e65f23c84688c30a8f
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-10 16:44:23 -0700

    Update setup.py

--- default/setup.py
+++ default/setup.py
@@ -2,22 +2,6 @@ import os
 import argparse
 
 
-def parser():
-    arg = argparse.ArgumentParser(description="Counting the entry and exit of people")
-    arg.add_argument("--YOLO",default="/darknet/",help="Directory to YOLO/Darknet")
-    arg.add_argument("--PATH_WEIGHTS",default="/darknet/backup/custom_tinyYOLOv4_final.weights")
-    arg.add_argument("--PATH_CFG",default="/darknet/custom_tinyYOLOv4.cfg")
-    arg.add_argument("--PATH_NAMES",default="/darknet/custom_data/obj.names")
-    arg.add_argument("--THRESHOLD",type=float,default=0.3,help="Minimum threshold for mask detection")
-    arg.add_argument("--CONFIDENCE",type=float,default=0.3,help="Minimum probability to filter weak detections")
-    arg.add_argument("--SKIP-FRAMES",type=int, default = 30, help = "Number of frames to skip in detection")
-    arg.add_argument("-s", "--skip-frames", type=int, default=30,help="# of skip frames between detections")
-    arg.add_argument("--VIDEO", default="/tracking-only/test.mp4",help="Video path")
-
-    args = vars(arg.parse_args())
-
-    return args
-
 #Using an MIPI CSI Camera needs opencv version that supports it otherwise the camera will not be
 # recognized using a gstreamper pipeline. ERROR -> Camera not detected 
 def gstreamer_pipeline(
@@ -48,3 +32,24 @@ def gstreamer_pipeline(
             display_height,
         )
     )
+
+
+def parser():
+
+    webcam = gstreamer_pipeline(flip_method=2)
+
+    arg = argparse.ArgumentParser(description="Counting the entry and exit of people")
+    arg.add_argument("--YOLO",default="/darknet/",help="Directory to YOLO/Darknet")
+    arg.add_argument("--PATH_WEIGHTS",default="/darknet/backup/custom_tinyYOLOv4_final.weights")
+    arg.add_argument("--PATH_CFG",default="/darknet/custom_tinyYOLOv4.cfg")
+    arg.add_argument("--PATH_NAMES",default="/darknet/custom_data/obj.names")
+    arg.add_argument("--THRESHOLD",type=float,default=0.3,help="Minimum threshold for mask detection")
+    arg.add_argument("--CONFIDENCE",type=float,default=0.3,help="Minimum probability to filter weak detections")
+    arg.add_argument("--SKIP-FRAMES",type=int, default = 30, help = "Number of frames to skip in detection")
+    arg.add_argument("-s", "--skip-frames", type=int, default=30,help="# of skip frames between detections")
+    arg.add_argument("--VIDEO", default=webcam,help="Video path")
+
+    args = vars(arg.parse_args())
+
+    return args
+
