bc0279b6103a109c913315e13df6cda37b8d0a82
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-10 02:01:13 -0700

    Update TrackAndDetect.py

--- util/TrackAndDetect.py
+++ util/TrackAndDetect.py
@@ -228,7 +228,7 @@ class Frame:
                 color = green
                 object.object_color_num_frame -= 1
 
-            cv2.circle(frame, round((x + x + w)/2),round((y+y+h)/2), 2, (0,0,0), 2)
+            cv2.circle(frame, (round((x + x + w)/2),round((y+y+h)/2)), 2, (0,0,0), 2)
             people_label = 'ID: ' + _id[:8] \
                             if object.type == None \
                             else 'ID: {0}, {1} ({2}%)'.format(_id[:8], object.type, str(round(object.type_confidence*100))[:4])
@@ -240,14 +240,14 @@ class Frame:
 
         # Display mask/people count
 
-        cv2.putText(frame, 'Masks worn in entry: ' + str(self.mask_on_enter), (10, 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(frame, 'Masks not worn in entry: ' + str(self.mask_off_enter), (10, 25), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Masks worn in entry: ' + str(self.mask_on_enter), (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Masks not worn in entry: ' + str(self.mask_off_enter), (10, 35), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
-        cv2.putText(frame, 'Masks worn in exit: ' + str(self.mask_on_enter), (10, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(frame, 'Masks not worn in exit: ' + str(self.mask_off_enter), (10, 55), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Masks worn in exit: ' + str(self.mask_on_enter), (10, 50), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Masks not worn in exit: ' + str(self.mask_off_enter), (10, 65), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
-        cv2.putText(frame, 'Count in: ' + str(self.person_in), (10, 70), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(frame, 'Count out: ' + str(self.person_out), (10, 85), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Count in: ' + str(self.person_in), (10, 80), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, 'Count out: ' + str(self.person_out), (10, 95), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
         #cv2.putText(frame, 'Processing speed: ' + str(self.frame_rate_processing) + ' FPS', (20, 180), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
         if self.show_counting:
