7e855c239fc61c2cd024d7fa2724d72c716a9dee
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-10 17:43:19 -0700

    Update coordinate_finder.py

--- default/coordinate_finder.py
+++ default/coordinate_finder.py
@@ -9,40 +9,43 @@ def get_box():
     '''
     first take a screenshot
     '''
-
-    
     args = parser()
-    video = "/tracking-only/test.mp4"
-    cap_ = cv2.VideoCapture(video)
+    cap_ = cv2.VideoCapture(args['VIDEO'])
     _, frame = cap_.read()
-    '''
-    
 
-    '''
-    cv2.imwrite('TestImage',frame)
+    W = None
+    H = None
+
     TakeScreenshot(frame,'TestImage')
 
-    
-    #Get bounding box/pixel positions
-    
+    '''
+    Get bounding box/pixel positions
+    '''
     filepath = 'images/TestImage.jpg'
     finder = BoundingBoxFinder(filepath)
 
     while True:
         img = finder.show_image()
         cv2.imshow('Reference Image', img)
+        
+        frame = cap_.read()
+        frame = frame[1]
 
-    
+        
+
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
         
         # Close program with keyboard 'q'
-        
-        #Upon exiting get the coordinates of the last bounding box
-        
+        '''
+        Upon exiting get the coordinates of the last bounding box
+        '''
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
+    
     cap_.release()
-    cv2.destroyAllWindows
+    cv2.destroyAllWindows()
     
     return finder.x, finder.y, finder.w, finder.h
     
