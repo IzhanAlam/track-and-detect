5f739e72fa0d0c815a84e991b99027f3113c1148
Izhan Alam <60088823+IzhanAlam@users.noreply.github.com>
2021-04-10 21:52:43 -0700

    updates

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def main():
 
         idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["CONFIDENCE"],args["THRESHOLD"])
         
-        
+        '''
         if len(idxs) > 0:
             for i in idxs.flatten():
                 #status = "Detecting"
@@ -99,7 +99,7 @@ def main():
                 text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
                 cv2.putText(frame, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 1)
         
-
+        '''
         counter.track_and_detect(frame, boxes, confidences, classIDs)
         output_frame = counter.show_result()
         
